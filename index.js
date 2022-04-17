36b2dc928fba676cbca97643c1ab14654c5aad4e
b1tc0re <b1tc0re@yandex.ru>
2022-04-17 03:14:18 +0300

    add ProjectBundles assets fix

--- index.js
+++ index.js
@@ -214,6 +214,7 @@ module.exports = {
                     sourceSuffixes: ['bemhtml', 'bemhtml.js'],
                     engineOptions : {
                         elemJsInstances : true,
+                        isDefineBemHtmlApi: true,
                         requires : {
                             'helper': {
                                 ym: 'helper',
